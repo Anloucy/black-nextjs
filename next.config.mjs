9ba576939f33a3616b1152f4429b4aa828827798
Luciano <lucianomagno.dev@gmail.com>
2024-08-19 16:53:12 -0300

    Obtendo produtos da Api(erro ao mostrar imagens)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  images: {
+    remotePatterns:[{
+      protocol: 'https',
+      hostname: 'store.storeimages.cdn-apple.com',
+      port: '',
+      pathname: '/**',
+    }],
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
